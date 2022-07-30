53fec0e43a0fdaeefd7eca57c2c8fe1dc51a6fe3
Alfred Tso <32275603+alfredtso@users.noreply.github.com>
2022-07-30 21:38:50 +0800

    simple cp and rm

---
+++ syncScanToPaperless.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+date
+
+cp ~/iCloudDrive/Documents/Scan/* ~/iCloudDrive/Documents/Archive
+cp ~/iCloudDrive/Documents/Scan/* ~/paperless-ng/consume
+rm ~/iCloudDrive/Documents/Scan/*
+
+
