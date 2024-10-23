74cd6cc4b76c5a0650be5cbd8c1de9f64e0c77e8
Alfred Tso tp <alfredtso@gmail.com>
2024-10-23 22:19:44 +0800

    add horizontal option

--- wlr.sh
+++ wlr.sh
@@ -2,7 +2,7 @@
 EXTERNAL_OUTPUT="HDMI-A-1"
 
 usage() {
-  echo "Usage: ${0##./} [display] (left|right)"
+  echo "Usage: ${0##./} [display] (left|right|horizontal)"
 }
 
 set_right() {
@@ -10,6 +10,12 @@ set_right() {
   wlr-randr --output ${1:?Specify the display} --transform 270 --pos 1920,0
   # Set it to the right of the main
   wlr-randr --output eDP-1 --pos 0,0
+}
+
+set_horizontal() {
+  # Set the Dell monitor as vertical
+  wlr-randr --output ${1:?Specify the display} --transform normal 
+  # Set it to the right of the main
 }
 
 set_left() {
@@ -26,7 +32,9 @@ fi
 
 if [[ $2 == "left" ]];then
   set_left "$1"
-else
+elif [[ $2 == "right" ]];then
   set_right "$1"
+elif [[ $2 == "horizontal" ]];then
+  set_horizontal "$1"
 fi
 
